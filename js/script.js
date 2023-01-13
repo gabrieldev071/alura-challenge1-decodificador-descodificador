ab9852f336f4e4a46c50ee9a25ca7bb1aa920e83
Gabriel Souza <imgabriel.souza071@gmail.com>
2023-01-13 20:22:40 -0300

    Atualizando script e estilização da página.

--- js/script.js
+++ js/script.js
@@ -1,19 +1,41 @@
 const inputTexto1 = document.querySelector(".input-texto-area1");
-const mensagem = document.querySelector(".container-input-texto-area2");
+const mensagem = document.querySelector(".input-texto-area2");
 
 
 function botaoEncriptar(){
-    const textEncriptado = encriptar(inputTexto1.value);
-    console.log("Teste do botão")
+    const textoEncriptado = encriptar(inputTexto1.value);
+    mensagem.value = textoEncriptado
+    console.log("Teste do botão");
 }
 
-/* As "chaves" de criptografia que utilizaremos são:
-A letra "e" é convertida para "enter"
-A letra "i" é convertida para "imes"
-A letra "a" é convertida para "ai"
-A letra "o" é convertida para "ober"
-A letra "u" é convertida para "ufat" */
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    console.table(matrizCodigo);
+    stringEncriptada = stringEncriptada.toLowerCase();
 
-function botaoDesncriptar(){
+    for(let i= 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+        stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada;
+}
 
+function botaoDesencriptar(){
+    const textoDesencriptado = desencriptar(inputTexto1.value);
+    mensagem.value = textoDesencriptado;
+    
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    console.table(matrizCodigo);
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i= 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][1])
+        }
+    }
+    return stringDesencriptada;
 }
